eecc41884d05d4146c0e501b841089666ccd4a91
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-07-07 19:40:16 +0600

    Initialize styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import './assets/scss/reset.scss';
+import './assets/scss/style.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <h1>Initial Commit</h1>
